6632f88e81300c0952aa41d0d4bf9d16b1789454
ip48 <innapolak@gmail.com>
2025-02-16 09:38:07 -0500

    task 8

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+# This is my final Flask project
+
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,16 +8,15 @@ app = Flask("My Final Project")
 
 @app.route("/emotionDetector")
 def detect_emotion():
-    textToAnalyze = request.args["textToAnalyze"]
-    resp = emotion_detector(textToAnalyze)
+    text_to_analyze = request.args["textToAnalyze"]
+    resp = emotion_detector(text_to_analyze)    
     if not resp['dominant_emotion']:
-        return "<b>Invalid text! Please try again!</b"
+        return "<b>Invalid text! Please try again!</b>"
 
     return f"For the given statement, the system response is 'anger': {resp['anger']},\
      'disgust': {resp['disgust']}, 'fear': {resp['fear']}, 'joy': {resp['joy']}\
       and 'sadness': {resp['sadness']}. \
-      The dominant emotion is <b>{resp['dominant_emotion']}.</b>"
-    
+      The dominant emotion is <b>{resp['dominant_emotion']}.</b>" 
 
 @app.route("/")
 def render_index_page():
